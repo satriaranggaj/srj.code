273ec949593b78e39f7909fb16027bcd8bea6352
satriaranggaj <satriarangga43@gmail.com>
2024-06-10 11:08:56 +0700

    npm build

--- app/Http/Controllers/DashboardController.php
+++ app/Http/Controllers/DashboardController.php
@@ -14,7 +14,7 @@ class DashboardController extends Controller
 {
     public function index()
     {
-        return view('admin.dashboard');
+        return view('Admin.dashboard');
     }
 
     // Skill Controller
